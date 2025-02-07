22c84cf010d722265d79cfc44627f1fe107f8d7f
Arun Balusamy <arun.balusamy@microchip.com>
2025-02-07 14:41:38 +0530

    [APP] Regenerated and tested the same70_tcm_performance on SAM E70 Xult | M32MM-3042

--- apps/sam_e70_xult/same70_tcm_performance/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/sam_e70_xult/same70_tcm_performance/firmware/src/config/sam_e70_xult/initialization.c
@@ -65,9 +65,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 11.1 */
-/* MISRA C-2012 Rule 11.3 */
-/* MISRA C-2012 Rule 11.8 */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
 
 
@@ -117,6 +118,7 @@ static void STDIO_BufferModeSet(void)
 
     /* Make stdout unbuffered */
     setbuf(stdout, NULL);
+    /* MISRAC 2012 deviation block end */
 }
 
 
@@ -158,16 +160,6 @@ void SYS_Initialize ( void* data )
     USART1_Initialize();
 
 
-
-    /* MISRAC 2012 deviation block start */
-    /* Following MISRA-C rules deviated in this block  */
-    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
-    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
-
-
-
-
-    /* MISRAC 2012 deviation block end */
     NVIC_Initialize();
 
 
